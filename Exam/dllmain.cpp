846c84e442eca3632c707dfb6663e3400636e04a
文成 杨 <boy_yec@hotmail.com>
2020-01-26 21:38:47 +0800

    手动创建ARX

--- Exam/dllmain.cpp
+++ Exam/dllmain.cpp
@@ -1,5 +1,14 @@
 ﻿// dllmain.cpp : 定义 DLL 应用程序的入口点。
+#include "stdafx.h"
 #include "pch.h"
+#include "rxregsvc.h"
+#include "aced.h"
+#include "tchar.h"
+
+void InitApplication();
+void UnloadApplication();
+void Hello();
+
 
 BOOL APIENTRY DllMain( HMODULE hModule,
                        DWORD  ul_reason_for_call,
@@ -17,3 +26,69 @@ BOOL APIENTRY DllMain( HMODULE hModule,
     return TRUE;
 }
 
+extern "C" AcRx::AppRetCode acrxEntryPoint(AcRx::AppMsgCode msg, void *pkt)
+{
+	switch (msg)
+	{
+	case AcRx::kNullMsg:
+		break;
+	case AcRx::kInitAppMsg:
+		acrxDynamicLinker->unlockApplication(pkt);
+		acrxDynamicLinker->registerAppMDIAware(pkt);
+		InitApplication();
+		break;
+	case AcRx::kUnloadAppMsg:
+		UnloadApplication();
+		break;
+	case AcRx::kLoadDwgMsg:
+		break;
+	case AcRx::kUnloadDwgMsg:
+		break;
+	case AcRx::kInvkSubrMsg:
+		break;
+	case AcRx::kCfgMsg:
+		break;
+	case AcRx::kEndMsg:
+		break;
+	case AcRx::kQuitMsg:
+		break;
+	case AcRx::kSaveMsg:
+		break;
+	case AcRx::kDependencyMsg:
+		break;
+	case AcRx::kNoDependencyMsg:
+		break;
+	case AcRx::kOleUnloadAppMsg:
+		break;
+	case AcRx::kPreQuitMsg:
+		break;
+	case AcRx::kInitDialogMsg:
+		break;
+	case AcRx::kEndDialogMsg:
+		break;
+	case AcRx::kSuspendMsg:
+		break;
+	case AcRx::kInitTabGroupMsg:
+		break;
+	case AcRx::kEndTabGroupMsg:
+		break;
+	default:
+		break;
+	}
+	return AcRx::kRetOK;
+}
+
+void Hello()
+{
+	acedPrompt(_T("\nHello World"));
+}
+
+void InitApplication()
+{
+	acedRegCmds->addCommand(_T("Exam01"), _T("Hello"), _T("你好"), ACRX_CMD_MODAL, Hello);
+}
+
+void UnloadApplication()
+{
+	acedRegCmds->removeGroup(_T("Exam01"));
+}
